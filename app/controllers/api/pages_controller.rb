77fcc9799c154f90de46f8264c3cc7cfdb20a786
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-04 17:40:58 +0300

    :construction: add validation and rescue

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,20 +1,32 @@
 module Api
   class PagesController < ApplicationController
+    before_action :get_params, except: :redis_ping
+
     def graph
-      raise 'missing argument \'keyword\'' if params[:keyword].blank?
       result = JSON.parse($redis.get(params[:keyword])) || {'graph' => {}}
       render json: result['graph']
+    rescue => e
+      render json: {message: e.message}, status: :internal_server_error
     end
 
     def data_graph
-      raise 'missing argument \'keyword\'' if params[:keyword].blank?
       result = JSON.parse($redis.get(params[:keyword])) || {'data_graph' => {}}
       render json: result['data_graph']
+    rescue => e
+      render json: {message: e.message}, status: :internal_server_error
     end
 
     def redis_ping
       $redis.set('ping', 'pong')
       render json: $redis.get('ping')
+    rescue => e
+      render json: {message: e.message}, status: :internal_server_error
     end
+
+    private
+
+    def get_params
+      raise 'missing argument \'keyword\'' if params[:keyword].blank?
+    end    
   end
 end
