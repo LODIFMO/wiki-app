834980c270454d9636aac10e9631b894939f22fb
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-04 17:05:45 +0300

    :hammer: clear code

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/show'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'pages#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-    namespace :api do
-      get 'pages/graph' => 'pages#graph', format: /json/
-      get 'pages/dgraph' => 'pages#data_graph', format: /json/
-    end
+  namespace :api do
+    get 'pages/graph' => 'pages#graph', format: /json/
+    get 'pages/dgraph' => 'pages#data_graph', format: /json/
+  end
 end
