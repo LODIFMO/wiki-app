ddca03d976fbe8cf3187c7b48b3c53639e445889
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-04 17:46:22 +0300

    :construction: add projects api

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -23,6 +23,18 @@ module Api
       render json: {message: e.message}, status: :internal_server_error
     end
 
+    def projects
+      keyword = JSON.parse $redis.get(params[:keyword])
+      if keyword.present? && keyword[:projects].present?
+        projects = keyword[:projects]
+      else
+        projects = []
+      end
+      render json: projects
+    rescue => e
+      render json: {message: e.message}, status: :internal_server_error
+    end
+
     private
 
     def get_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
     get 'pages/graph' => 'pages#graph', format: /json/
     get 'pages/dgraph' => 'pages#data_graph', format: /json/
 
+    # ping redis
     get 'pages/redis/ping' => 'pages#redis_ping', format: /json/
+
+    # get data
+    get 'projects' => 'pages#projects', format: /json/
   end
 end
