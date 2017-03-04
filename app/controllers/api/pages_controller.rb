11e33dcbaa84331f78f4e50e569eb8b5bd828bd7
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-04 17:01:46 +0300

    :construction: add data graph api

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,8 +2,14 @@ module Api
   class PagesController < ApplicationController
     def graph
       raise 'missing argument \'keyword\'' if params[:keyword].blank?
-      result = $redis.get(params[:keyword]) || {graph: {}}
-      render json: result
+      result = JSON.parse($redis.get(params[:keyword])) || {'graph' => {}}
+      render json: result['graph']
+    end
+
+    def data_graph
+      raise 'missing argument \'keyword\'' if params[:keyword].blank?
+      result = JSON.parse($redis.get(params[:keyword])) || {'data_graph' => {}}
+      render json: result['data_graph']
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -51,5 +51,6 @@ Rails.application.routes.draw do
 
     namespace :api do
       get 'pages/graph' => 'pages#graph', format: /json/
+      get 'pages/dgraph' => 'pages#data_graph', format: /json/
     end
 end
